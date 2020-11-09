90b3e3a1f4d60ab5de59f634873a7ec7110cc561
Gaurav Chemburkar <gaurav.mrc@gmail.com>
2020-11-09 22:51:19 +0530

    base path changed to /launches

--- components/FilterContainer/FilterContainer.js
+++ components/FilterContainer/FilterContainer.js
@@ -4,7 +4,7 @@ const filterContainer = () => {
     return(
         <>
             <h1>Filters</h1>
-            <Link href="/launch?launch_year=2014">
+            <Link href="?launch_year=2014">
                 <a>Year 2014</a>
             </Link>
         </>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,28 @@ import FilterContainer from "../components/FilterContainer/FilterContainer";
 import ResultContainer from "../components/ResultContainer/ResultContainer";
 
 const Index = ({data}) => {
-   return <div>Index</div>;
+    return (
+        <>
+            <FilterContainer />
+            <ResultContainer data = {data} />
+        </>
+    );
+}
+
+export const getServerSideProps = async ctx => {
+    // Fetch data from external API
+    const {query} = ctx;
+    console.log(query);
+    if(query.launch_year){
+        var api = `https://api.spaceXdata.com/v3/launches?limit=2&launch_year=${query.launch_year}`;
+    }else{
+        var api = `https://api.spaceXdata.com/v3/launches?limit=2`;
+    }
+    const res = await fetch(api);
+    const data = await res.json()
+    //console.log("fetch data",data);
+    // Pass data to the page via props
+    return { props: { data } }
 }
 
 export default Index;
