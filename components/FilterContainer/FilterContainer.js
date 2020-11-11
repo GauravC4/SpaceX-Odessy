283e87218e283c16538ee8387e7e8c2297804c35
Gaurav Chemburkar <gaurav.mrc@gmail.com>
2020-11-11 20:47:01 +0530

    added launch year filter stub

--- components/FilterContainer/FilterContainer.js
+++ components/FilterContainer/FilterContainer.js
@@ -6,6 +6,14 @@ const filterContainer = () => {
     const booleanFilters = [{label: "True", value: true}, {label: "False", value: false}];
     const LAUNCH_FILTER = "LAUNCH";
     const LAND_FILTER = "LAND";
+    const YEAR_FILTER = "YEAR";
+    const START_YEAR = 2006;
+    const CURR_YEAR = new Date().getFullYear();
+
+    let yearFilters = [];
+    for(let i = START_YEAR; i <= CURR_YEAR; i++){
+        yearFilters.push({label: i+"", value: i});
+    } 
 
     const onFilterSet = (name, filterType) => { 
         console.log(`${filterType} filter set with ${name}.`);
@@ -18,6 +26,13 @@ const filterContainer = () => {
     return(
         <div className={styles.filter_container}>
             <h1>Filters</h1>
+            <h3>Launch year</h3>
+            <FilterInputGroup 
+                filters = {yearFilters}
+                onFilterSet = {onFilterSet}
+                onFilterClear = {onFilterClear}
+                filterType = {YEAR_FILTER}
+            />
             <h3>Successful Launch</h3>
             <FilterInputGroup 
                 filters = {booleanFilters}
